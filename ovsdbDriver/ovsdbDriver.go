d7c5313f8ecc4504fe7a07b8f9548f5df6813ebb
chengcheng.luo <chengcheng.luo@smartx.com>
2021-03-19 03:21:49 -0400

    init ovsDirver with already exists bridge

--- ovsdbDriver/ovsdbDriver.go
+++ ovsdbDriver/ovsdbDriver.go
@@ -63,6 +63,33 @@ func NewOvsDriver(bridgeName string) *OvsDriver {
 	return ovsDriver
 }
 
+func InitOvsDriverWithBridge(bridgeName string) *OvsDriver {
+	ovsDriver := new(OvsDriver)
+
+	// connect to OVS
+	ovs, err := libovsdb.ConnectUnix("/var/run/openvswitch/db.sock")
+	if err != nil {
+		log.Fatal("Failed to connect to ovsdb. Err: ", err)
+	}
+
+	// Setup state
+	ovsDriver.ovsClient = ovs
+	ovsDriver.OvsBridgeName = bridgeName
+	ovsDriver.ovsdbCache = make(map[string]map[string]libovsdb.Row)
+
+	go func() {
+		// Register for notifications
+		ovs.Register(ovsDriver)
+
+		// Populate initial state into cache
+		initial, _ := ovs.MonitorAll("Open_vSwitch", "")
+		ovsDriver.populateCache(*initial)
+	}()
+
+	// Return the new OVS driver
+	return ovsDriver
+}
+
 // Delete : Cleanup the ovsdb driver. delete the bridge we created.
 func (d *OvsDriver) Delete() error {
 	if d.ovsClient != nil {
